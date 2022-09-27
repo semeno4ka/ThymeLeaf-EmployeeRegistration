3e6fcd3fc6dbc138a11205a33d64ad404bf2e4b7
Ekaterina <ekaterina.sementeva@gmail.com>
2022-09-27 13:10:03 -0400

    Employee Registration Form Data connection

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,17 +1,35 @@
 package com.cydeo.controller;
 
+import com.cydeo.bootstrap.DataGenerator;
+import com.cydeo.model.Employee;
+import com.cydeo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
+// USe dependency injection to USe Employee Service to Save data
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
     @GetMapping("/register")
-    public String createEmployee(Model model){//bring up the page
-
+    public String createEmployee(Model model){//bring data to view
+        model.addAttribute("employee",new Employee());
+        model.addAttribute("stateList", DataGenerator.getAllStates());
         return "/employee/employee-create";
     }
+
+    @PostMapping("/insert")
+        public String insertEmployee(@ModelAttribute("employee") Employee employee){
+                employeeService.saveEmployee(employee);
+                return "/employee/employee-list";
+        }
 }
--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -3,6 +3,7 @@ package com.cydeo.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 @Data
@@ -12,6 +13,9 @@ public class Employee {
 
     private String firstName;
     private String lastName;
+    //ThymeLeaf accepts yyyy-MM-dd this format when saves data, but LocalDate accepts mm-dd-yyyy
+    //That is why formatting required
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
     private String email;
     private String password;
