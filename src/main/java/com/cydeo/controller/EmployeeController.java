047712d53dc7e1a82b35c46a5a035d3125de79da
Ekaterina <ekaterina.sementeva@gmail.com>
2022-09-27 15:48:47 -0400

    Validation

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -5,10 +5,13 @@ import com.cydeo.model.Employee;
 import com.cydeo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -28,7 +31,16 @@ public class EmployeeController {
     }
 
     @PostMapping("/insert")
-        public String insertEmployee(@ModelAttribute("employee") Employee employee) {
+        public String insertEmployee(@ModelAttribute("employee") @Valid Employee employee, BindingResult bindingResult, Model model) {
+        //Binding result should be placed right after the class object you check for validation. This Interface helps to connect validation with result
+        if(bindingResult.hasErrors()){// if there is an error
+            model.addAttribute("stateList", DataGenerator.getAllStates());
+            // we again add model stateList so that our page stays the
+            // same in case of error and there will be no need to input all data all over again.
+            // If we do not add it, states are lost, because they are at different end point
+            return "employee/employee-create";
+        }
+        //Can create custom validation if needed
         employeeService.saveEmployee(employee);
         return "redirect:/employee/list";
     }
@@ -38,4 +50,24 @@ public class EmployeeController {
             model.addAttribute("employeeList",employeeService.readAllEmployees());
             return  "/employee/employee-list";
         }
+        /*Validation. How to prevent user from unacceptable inputs
+    1 Step: pomxml add dependency validation:
+           <dependency>
+           <groupId>org.springframework.boot</groupId>
+           <artifactId>spring-boot-starter-validation</artifactId>
+           </dependency>
+    2  Step: decide what limitations will be required
+       go to Class and add required annotations, such as
+@NotNull // field cannot be null
+@NotBlank // cannot be left blank -> "  " -> String str="  "
+@NotEmpty // should contain data  -> ""   -> String str=""
+@Size(min=   max=)
+
+All feilds have their own validation annotation
+
+Controller SHOULD KNOW about annotations and validations: You should @Valid in the method using the Class Onject, such as
+@ModelAttribute("employee") @Valid Employee employee (inside postmapping, because we check info that the user sends us)
+
+    */
+
 }
--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -5,12 +5,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor// for an empty object in the form while its blanks
 public class Employee {
-
+/*@NotNull field cannot be null
+@NotBlank  cannot be left blank -> "  " -> String str="  " (Covers All Null and Empty)
+@NotEmpty  should contain data  -> ""   -> String str=""   (Covers Null)*/
+    @NotBlank
+    @Size(max=12, min=2)
     private String firstName;
     private String lastName;
     //ThymeLeaf accepts yyyy-MM-dd this format when saves data, but LocalDate accepts mm-dd-yyyy
