93556c5c37b6932ff48e611c8a4795c27cec12af
Ekaterina <ekaterina.sementeva@gmail.com>
2022-09-27 11:16:38 -0400

    Employee Registration Start

---
+++ src/main/java/com/cydeo/ThymeLeafEmployeeRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafEmployeeRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeLeafEmployeeRegistrationApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cydeo/model/Employee.java
@@ -0,0 +1,24 @@
+package com.cydeo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor// for an empty object in the form while its blanks
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+
+}
